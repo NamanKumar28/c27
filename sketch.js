942445e58a668d47ea792658f43fb15ee1ea0296
NamanKumar28 <godrocknamankk@gmail.com>
2020-07-24 16:50:10 +0530

    addedAConstraint

--- sketch.js
+++ sketch.js
@@ -34,7 +34,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(230,180,80,PI/2);
+
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -58,7 +62,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    chain.display();
 }
